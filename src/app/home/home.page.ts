e47bcd58d9fe56152b3c004a37b816a9e5e978a5
masooanjos <125562535+masooanjos@users.noreply.github.com>
2023-06-27 14:41:29 +0000

    clearCommit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,29 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+
+	resultado: any = 0;
+
+	exibir(numero: any) {
+		// usamos this para acessar a variável que foi criada acima
+		this.resultado += numero;
+	}
+
+  clear(){
+    this.resultado = '';
+
+  }
+
+  apagaUltimo(){
+    let chars = this.resultado.slice(0, -1);
+    this.resultado = chars;
+  }
+
+  somar(){
+    
+  }
+
 }
