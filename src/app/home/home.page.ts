d3d245ca60c90831880cc17fdbf126009895f3fa
masooanjos <125562535+masooanjos@users.noreply.github.com>
2023-06-28 13:51:31 +0000

    commitFinal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,23 +14,28 @@ export class HomePage {
   //criação da variável que exibe a expressão
   igual: any = "";
   //criação da variável que exibe o resultado
-	resultado: any = "";
-  
+  resultado: any = "";
+
   //função que recebe os números e exibe o resultado
-	exibir(numero: any) {
-		// usamos this para acessar a variável que foi criada acima
-		this.igual += numero;
+  exibir(numero: any) {
+    //verifica se digitou vários pontos seguidos
+    if(this.igual[this.igual.length-1] == "." && numero == "."){
+      return;
+    }
+      
+    // usamos this para acessar a variável que foi criada acima
+    this.igual += numero;
     this.atualizaResultado();
-	}
+  }
 
   //função que limpa todos os arrays
-  clear(){
+  clear() {
     this.igual = '';
     this.resultado = '';
   }
 
   //função que apaga o último número digitado/exibido
-  apagaUltimo(){
+  apagaUltimo() {
     let chars = this.igual.slice(0, -1);
     this.igual = chars;
     this.resultado = chars;
@@ -38,11 +43,11 @@ export class HomePage {
   }
 
   //função que inverte o sinal do número (+/-)
-  inverterSinal(){
+  inverterSinal() {
     //o split está dividindo a expressão em partes diferentes
     this.igual = this.igual.split(/([-+*/])/);
     //inverte o sinal do último elemento
-    let ultimo = Number(this.igual[this.igual.length-1]) * (-1);
+    let ultimo = Number(this.igual[this.igual.length - 1]) * (-1);
     //o pop elimina o último elemento
     this.igual.pop()
     //o join junta os elementos novamente
@@ -52,45 +57,50 @@ export class HomePage {
   }
 
   //essa função realiza as expressões(soma, subtração, multiplicação e divisão)
-  operadores(numero: any){
+  operadores(numero: any) {
     //se o array estiver vazio ele não deixa colocar operadores além do sinal de subtração(-)
-    if(this.igual.length == 0 && numero != "-"){
+    if (this.igual.length == 0 && numero != "-") {
       return;
     }
     //proíbe de colocar operadores sequencialmente
-    if (["-", "+", "*", "/"].includes(this.igual[this.igual.length-1])) {
+    if (["-", "+", "*", "/"].includes(this.igual[this.igual.length - 1])) {
       this.apagaUltimo();
     }
     this.igual += numero;
     this.atualizaResultado();
-
   }
 
   //gera, calcula e exibe o resultado da expressão
-  botaoIgual(){
+  botaoIgual() {
+    //o eval calcula as expressões
     this.resultado = eval(this.igual);
   }
 
   //função que gera cálculo da porcentagem
-  porcentagem(){
-  //o split está dividindo a expressão em partes diferentes
-  this.igual = this.igual.split(/([-+*/])/);
-  //faz a porcentagem se tiver apenas um número
-  if(this.igual.length == 1){
-    this.resultado = this.igual / 100;
-    return;
-  }
+  porcentagem() {
+    //o split está dividindo a expressão em partes diferentes
+    this.igual = this.igual.split(/([-+*/])/);
+    //faz a porcentagem se tiver apenas um número
+    if (this.igual.length == 1) {
+      this.resultado = this.igual / 100;
+      return;
+    }
 
-  //gera a porcentagem com números na frente, ex: 80+5%
-  let porcento = (this.igual[this.igual.length-1]) / 100;
-  this.igual.pop();
-  this.igual = this.igual.join("");
-  let aux = this.igual.split(/([-+*/])/);
-  this.igual += Number(aux[aux.length-3]) * porcento;
+    //gera a porcentagem com números na frente, ex: 80+5%
+    let porcento = (this.igual[this.igual.length - 1]) / 100;
+    //o pop elimina o último elemento
+    this.igual.pop();
+    //o join junta os elementos novamente
+    this.igual = this.igual.join("");
+    //o split está dividindo a expressão em partes diferentes
+    let aux = this.igual.split(/([-+*/])/);
+    //faz o calcula da porcentagem do número escolhido
+    this.igual += Number(aux[aux.length - 3]) * porcento;
   }
 
   //função que atualiza o resultado
   atualizaResultado() {
+    //o eval calcula as expressões
     this.resultado = eval(this.igual);
   }
 }
